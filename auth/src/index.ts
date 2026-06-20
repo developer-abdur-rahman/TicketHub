211b27d55a00aa224b4a9d7b9eb9d8989f6bf86d
developer-abdur-rahman <dev.abdurrahman@proton.me>
2026-06-20 23:17:35 +0600

    ingress-nginx service added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get("/api/users/me", (req, res) => {
+  console.log("I am hit.")
+  res.send("This is your profile");
+});
+
 app.listen(5000, () => {
   console.log("Auth service is running on port 5000 ...");
 });
