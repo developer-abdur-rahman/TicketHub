e5a0a413449f63d70b99cc998efaa16e5491c11c
developer-abdur-rahman <dev.abdurrahman@proton.me>
2026-06-21 18:38:26 +0600

    organize all route for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import { currentUserRouter } from "./route/current-user.js";
+import { signin } from "./route/signin.js";
+import { signout } from "./route/signout.js";
+import { signup } from "./route/signup.js";
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/api/users/me", (req, res) => {
-  console.log("I am hit.")
-  res.send("This is your profile");
-});
+app.use(currentUserRouter);
+app.use(signin);
+app.use(signout);
+app.use(signup);
 
 app.listen(5000, () => {
   console.log("Auth service is running on port 5000 ...");
